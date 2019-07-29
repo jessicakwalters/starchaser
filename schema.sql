e4d944a46c3fb4e3a2ef5225bfecc1e12dd183e4
ginapult <gina.pultorak@gmail.com>
2019-07-29 13:06:00 -0700

    database and table created for dark parks

---
+++ schema.sql
@@ -0,0 +1,12 @@
+DROP TABLE IF EXISTS dark_parks;
+
+CREATE TABLE dark_parks (
+  id SERIAL PRIMARY KEY,
+  park_name VARCHAR(255),
+  location_name VARCHAR(255),
+  lat NUMERIC(10, 7),
+  long NUMERIC(10, 7),
+  img_url VARCHAR(255),
+  learn_more_url VARCHAR(255),
+  idsa_desig VARCHAR(255)
+);
